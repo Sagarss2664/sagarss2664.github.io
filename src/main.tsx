59bb93298da55f662439e0eabe20ce05a69bbaa4
Sagarss2664 <01fe22bcs259@kletech.ac.in>
2025-08-24 22:42:47 +0530

    Installed EmailJS and updated config

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,20 @@
 
+// import React from 'react';
+// import { createRoot } from 'react-dom/client';
+// import App from './App.tsx';
+// import './index.css';
+
+// const root = document.getElementById('root');
+// if (root) {
+//   createRoot(root).render(
+//     <React.StrictMode>
+//       <App />
+//     </React.StrictMode>
+//   );
+// }
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { HashRouter } from 'react-router-dom';
 import App from './App.tsx';
 import './index.css';
 
@@ -8,7 +22,9 @@ const root = document.getElementById('root');
 if (root) {
   createRoot(root).render(
     <React.StrictMode>
-      <App />
+      <HashRouter>
+        <App />
+      </HashRouter>
     </React.StrictMode>
   );
 }
